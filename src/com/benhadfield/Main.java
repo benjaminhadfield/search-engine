d80587000f4a30614a4ba332f94a150691b9da8d
Ben Hadfield <Ben@hadfieldfamily.co.uk>
2016-11-24 16:15:54 +0000

    Added Mapper functionality

--- src/com/benhadfield/Main.java
+++ src/com/benhadfield/Main.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Mapper mapper = new Mapper("./data/simple.txt");
+        Mapper mapper = new Mapper("./data/numbers.txt");
         mapper.generateMap();
 
         mapper._printMap();
