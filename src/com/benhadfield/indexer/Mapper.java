2de8321f5b36a5ab5385dcfa685dfcfe2964c8c9
Ben Hadfield <Ben@hadfieldfamily.co.uk>
2016-11-24 15:42:53 +0000

    created indexer classes

---
+++ src/com/benhadfield/indexer/Mapper.java
@@ -0,0 +1,9 @@
+package com.benhadfield.indexer;
+
+/**
+ * The Mapper class is responsible for creating postings, keyed by terms.
+ */
+
+public class Mapper {
+
+}
---
+++ src/com/benhadfield/indexer/Reducer.java
@@ -0,0 +1,9 @@
+package com.benhadfield.indexer;
+
+/**
+ * The Reducer class is responsible for writing postings lists to disk.
+ */
+
+public class Reducer {
+
+}
