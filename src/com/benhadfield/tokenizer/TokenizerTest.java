129a8e65f7bd1a955e7b85b48e6f3b978834751d
Ben Hadfield <Ben@hadfieldfamily.co.uk>
2016-11-25 11:35:21 +0000

    implemented first JUnit5 test

---
+++ src/com/benhadfield/tokenizer/TokenizerTest.java
@@ -0,0 +1,21 @@
+package com.benhadfield.tokenizer;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+/*
+ * Created by Moose on 25/11/2016.
+ */
+
+class TokenizerTest {
+    @Test
+    void getTokens() {
+        assertEquals(1, 2 - 1);
+    }
+
+    @Test
+    void generateTokens() {
+        assertEquals(10, 2 * 5);
+    }
+
+}
