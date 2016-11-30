5ea422d1eab40462c127ab2d9f36177f973ceb0d
Ben Hadfield <Ben@hadfieldfamily.co.uk>
2016-11-30 00:09:53 +0000

    added some comments / readme clarifications

--- src/com/benhadfield/retriever/TermLocation.java
+++ src/com/benhadfield/retriever/TermLocation.java
@@ -2,15 +2,13 @@ package com.benhadfield.retriever;
 
 /**
  * The TermLocation class is responsible for holding the location of a term's postings list as output by a reducer.
+ *
+ * Since this class is only used by the Retriever, lets constrain access to this package only.
  */
 
 class TermLocation {
     private String path;
     private long offset;
-
-    /**
-     * Since TermLocation is only used by the Retriever, lets constrain access to this package only.
-     */
 
     TermLocation(String path, long offset) {
         this.path = path;
